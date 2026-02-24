832f4fd1d561fd7d068dfc00a729d4fa127f61fd
Joseph Glynn <josephnglynn@hotmail.co.uk>
2026-02-24 13:14:40 +0000

    Add JavaDoc Comments

--- backend/src/main/java/uk/ac/warwick/dcs/airportsimulator/eventlog/EventType.java
+++ backend/src/main/java/uk/ac/warwick/dcs/airportsimulator/eventlog/EventType.java
@@ -1,5 +1,9 @@
 package uk.ac.warwick.dcs.airportsimulator.eventlog;
 
+/**
+ * Event type enum containing all event types
+ * which will be stored in the event log
+ */
 public enum EventType {
     LANDING_EVENT,
     TAKEOFF_EVENT,
