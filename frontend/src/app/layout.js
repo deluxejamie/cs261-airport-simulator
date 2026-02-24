7af2dc6a6866b3b716f3f90053a03709435d4d8a
Jamie <deluxe@jaiii.me>
2026-02-24 11:04:25 +0000

    chore: fixed packages

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,28 +1,25 @@
-// Import styles of packages that you've installed.
-// All packages except `@mantine/hooks` require styles imports
 import "@mantine/core/styles.css";
 import "./globals.css";
-
 import {
-  ColorSchemeScript,
-  MantineProvider,
-  mantineHtmlProps,
+	ColorSchemeScript,
+	MantineProvider,
+	mantineHtmlProps,
 } from "@mantine/core";
 
 export const metadata = {
-  title: "Airport Simulator",
-  description: "CS261 Group 4 Coursework for Dorset Software",
+	title: "Airport Simulator",
+	description: "CS261 Group 4 Coursework for Dorset Software",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en" {...mantineHtmlProps}>
-      <head>
-        <ColorSchemeScript />
-      </head>
-      <body>
-        <MantineProvider>{children}</MantineProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" {...mantineHtmlProps}>
+			<head>
+				<ColorSchemeScript />
+			</head>
+			<body>
+				<MantineProvider>{children}</MantineProvider>
+			</body>
+		</html>
+	);
 }
