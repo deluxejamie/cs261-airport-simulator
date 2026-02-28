24bde39b7e5b854e108e382c57b5e9d393bf7542
Jamie <deluxe@jaiii.me>
2026-02-28 15:34:51 +0000

    feat: implements export button

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -5,11 +5,21 @@ import {
 	RunwayModes,
 	useAdvancedConfig,
 	useFlightSchedule,
+	useHazardSchedule,
 } from "./hooks";
+import ExportImportButtons from "@/components/export-import";
 
 export default function Home() {
 	const { runways, addRunway, removeRunway } = useRunways();
 	const { advancedConfig } = useAdvancedConfig();
 	const { flights } = useFlightSchedule();
-	return <></>;
+	const { hazards } = useHazardSchedule();
+	return (
+		<ExportImportButtons
+			runways={runways}
+			advancedConfig={advancedConfig}
+			flights={flights}
+			hazards={hazards}
+		/>
+	);
 }
