410dbe55b73a36b21f2f983ca948ebe153575726
Jamie <deluxe@jaiii.me>
2026-02-28 15:32:48 +0000

    Revert "feat: implements export button"
    This reverts commit 0e239a43e97ba78487369db9f3169a8718c03865.

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -5,21 +5,11 @@ import {
 	RunwayModes,
 	useAdvancedConfig,
 	useFlightSchedule,
-	useHazardSchedule,
 } from "./hooks";
-import ExportImportButtons from "@/components/export-import";
 
 export default function Home() {
 	const { runways, addRunway, removeRunway } = useRunways();
 	const { advancedConfig } = useAdvancedConfig();
 	const { flights } = useFlightSchedule();
-	const { hazards } = useHazardSchedule();
-	return (
-		<ExportImportButtons
-			runways={runways}
-			advancedConfig={advancedConfig}
-			flights={flights}
-			hazards={hazards}
-		/>
-	);
+	return <></>;
 }
