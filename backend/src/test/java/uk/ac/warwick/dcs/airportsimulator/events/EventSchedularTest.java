5cb4f687628f8ee4b1e97fcc9a61208c24f31fc8
Kenneth1129 <157056087+Kenneth939@users.noreply.github.com>
2026-03-01 15:28:40 +0000

    SCRUM-10 unit tests

--- backend/src/test/java/uk/ac/warwick/dcs/airportsimulator/events/EventSchedularTest.java
+++ backend/src/test/java/uk/ac/warwick/dcs/airportsimulator/events/EventSchedularTest.java
@@ -1,8 +1,9 @@
 package uk.ac.warwick.dcs.airportsimulator.events;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,68 +11,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class EventSchedularTest {
 
     @Test
-    void addEvent_and_step_shouldExecuteEventsInTimeOrder() {
+    void step_shouldExecuteEventsInTimeOrder() {
         EventSchedular s = new EventSchedular();
 
-        AtomicInteger counter = new AtomicInteger(0);
+        List<String> order = new ArrayList<>();
 
-        s.addEvent(new PlainEvent(10, counter::incrementAndGet));
-
-        s.addEvent(new PlainEvent(5, counter::incrementAndGet));
+        s.addEvent(new PlainEvent(10, () -> order.add("A")));
+        s.addEvent(new PlainEvent(5, () -> order.add("B")));
 
         s.step(4);
-        assertEquals(0, counter.get());
+        assertTrue(order.isEmpty());
 
         s.step(5);
-        assertEquals(1, counter.get());
+        assertEquals(List.of("B"), order);
 
         s.step(10);
-        assertEquals(2, counter.get());
+        assertEquals(List.of("B", "A"), order);
     }
 
     @Test
-    void step_shouldRescheduleRecurringEventsUsingNext_untilEndTime() {
+    void step_shouldRescheduleRecurringEventsUntilEndTimeExclusive() {
         EventSchedular s = new EventSchedular();
 
         AtomicInteger counter = new AtomicInteger(0);
 
-        s.addEvent(new PlainEvent(0, 5, 15, counter::incrementAndGet));
+        // PlainEvent.next stops when nextTime >= endTime
+        // With endTime=16, occurrences at 0,5,10,15 (next would be 20 >= 16 => null)
+        s.addEvent(new PlainEvent(0, 5, 16, counter::incrementAndGet));
 
-        s.step(0);
-        assertEquals(1, counter.get());
-
-        s.step(5);
-        assertEquals(2, counter.get());
-
-        s.step(15);
-        assertEquals(3, counter.get());
-
-        s.step(20);
-        assertEquals(3, counter.get());
-    }
-
-
-
-    @Test
-    void testStepWithNormDistEvents()
-    {
-        EventSchedular s = new EventSchedular();
-
-        AtomicInteger i = new AtomicInteger();
-
-        s.addEvent(new NormDistEvent(0, 0, i::getAndIncrement));
-        s.addEvent(new NormDistEvent(100, 15, 160, 1, i::getAndIncrement));
-
-        assertEquals(0, i.get());
-        s.step(10);
-        assertEquals(1, i.get());
-
-        for (int j = 100; j <= 160; j += 15)
-        {
-            s.step(j);
-        }
-
-
-        assertEquals(5, i.get());
+        s.step(100); // large simTime should process all due events in one go
+        assertEquals(4, counter.get());
     }
 }
