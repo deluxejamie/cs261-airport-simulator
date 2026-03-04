4848fded3d808175670b095c1fcd558d37866727
Yasvi Tank <yasvitank@gmail.com>
2026-03-04 13:26:45 +0000

    Add test for useRunways

--- frontend/src/app/hooks.test.js
+++ frontend/src/app/hooks.test.js
@@ -9,3 +9,58 @@ describe("runway custom hook", () => {
 		expect(runways).toEqual([]);
 	});
 });
+
+describe("useRunways", () => {
+	it("should start empty", () => {
+	  const { result } = renderHook(() => useRunways());
+	  expect(result.current.runways).toEqual([]);
+	});
+  
+	it("should add runway", () => {
+	  const { result } = renderHook(() => useRunways());
+  
+	  act(() => {
+		result.current.addRunway(RunwayModes.MIXED_MODE);
+	  });
+  
+	  expect(result.current.runways.length).toBe(1);
+	});
+  
+	it("should remove runway", () => {
+	  const { result } = renderHook(() => useRunways());
+  
+	  act(() => {
+		result.current.addRunway(RunwayModes.MIXED_MODE);
+	  });
+  
+	  const id = result.current.runways[0].id;
+  
+	  act(() => {
+		result.current.removeRunway(id);
+	  });
+  
+	  expect(result.current.runways).toEqual([]);
+	});
+  
+	it("should throw on invalid mode", () => {
+	  const { result } = renderHook(() => useRunways());
+  
+	  expect(() =>
+		result.current.addRunway("invalid")
+	  ).toThrow();
+	});
+  
+	it("should not allow more than 10 runways", () => {
+	  const { result } = renderHook(() => useRunways());
+  
+	  act(() => {
+		for (let i = 0; i < 10; i++) {
+		  result.current.addRunway(RunwayModes.MIXED_MODE);
+		}
+	  });
+  
+	  expect(() =>
+		result.current.addRunway(RunwayModes.MIXED_MODE)
+	  ).toThrow();
+	});
+  });
