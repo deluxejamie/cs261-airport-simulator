3322e01feefc80eab04a1201d4fbd8538cb2af3a
Jamie <deluxe@jaiii.me>
2026-03-06 16:21:36 +0000

    feat: simulation playback controls

--- frontend/src/components/simulation-outcome-viewer.jsx
+++ frontend/src/components/simulation-outcome-viewer.jsx
@@ -12,18 +12,22 @@ import {
 	Table,
 	Text,
 	Title,
+	Slider,
 } from "@mantine/core";
 import {
 	getSimulationEventLog,
 	getSimulationResult,
 	getSimulationStatus,
 } from "@/lib/simulation-api";
+import { IconPlayerPlay } from "@tabler/icons-react";
+
+const DEFAULT_PLAY_SPEED = 20; // 20 minutes = 1 second of playthrough
 
 export default function SimulationOutcomeFoundPage({ uuid }) {
 	return (
 		<>
-			<SimulationEventLogComponent uuid={uuid} />
 			<SimulationStatsComponent uuid={uuid} />
+			<SimulationEventLogComponent uuid={uuid} />
 		</>
 	);
 }
@@ -34,8 +38,47 @@ export default function SimulationOutcomeFoundPage({ uuid }) {
  */
 const SimulationEventLogComponent = ({ uuid }) => {
 	// display the simulation event log (SCRUM-37)
-
-	return <></>;
+	const [currentEvents, setCurrentEvents] = useState([]);
+	const [speed, setSpeed] = useState(DEFAULT_PLAY_SPEED); // number of minutes displayed per second of playthrough
+	const [running, setRunning] = useState(false);
+	// References used:
+	// https://mantine.dev/core/slider/
+	return (
+		<>
+			<Title order={2}>Event Log</Title>
+			<Card withBorder radius="md" p="lg">
+				<Stack>
+					<Group justify="space-between">
+						<Title order={4}>Configure Simulation Playback</Title>
+						<Text size="lg">Speed: {speed} minutes simulated/second</Text>
+					</Group>
+					<Slider
+						color="blue"
+						pb="xl"
+						domain={[0, 100]}
+						min={10}
+						max={100}
+						defaultValue={DEFAULT_PLAY_SPEED}
+						onChangeEnd={setSpeed}
+						disabled={running}
+						marks={[
+							{ value: 10, label: "10" },
+							{ value: 40, label: "40" },
+							{ value: 70, label: "70" },
+							{ value: 100, label: "100" },
+						]}
+					/>
+					<Button
+						fullWidth
+						variant="light"
+						leftSection={<IconPlayerPlay size={18} />}
+					>
+						Start Event Simulation
+					</Button>
+				</Stack>
+			</Card>
+		</>
+	);
 };
 
 /**
