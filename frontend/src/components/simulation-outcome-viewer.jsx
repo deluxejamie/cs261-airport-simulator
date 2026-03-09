678f1a6de8c85875814b7572ff91f95590ab7122
Jamie <deluxe@jaiii.me>
2026-03-09 00:03:59 +0000

    feat: skeleton for event log

--- frontend/src/components/simulation-outcome-viewer.jsx
+++ frontend/src/components/simulation-outcome-viewer.jsx
@@ -33,16 +33,37 @@ export default function SimulationOutcomeFoundPage({ uuid }) {
 }
 
 /**
+ * This function generates a Card to display in the event log using the provided event data
+ * @param {object} event An event which
+ * @returns {} A react component
+ */
+const generateEventCard = (event) => {
+	return <>temp card</>;
+};
+
+/**
  * @param {{ uuid: String}} param0 The uuid for the simulation
  * @returns A react component
  */
 const SimulationEventLogComponent = ({ uuid }) => {
 	// display the simulation event log (SCRUM-37)
-	const [currentEvents, setCurrentEvents] = useState([]);
+	const [eventsFromSvr, setEventsFromSvr] = useState([
+		{ id: 1, time: 0 },
+		{ id: 2, time: 0 },
+		{ id: 3, time: 0 },
+		{ id: 4, time: 25 },
+	]);
 	const [speed, setSpeed] = useState(DEFAULT_PLAY_SPEED); // number of minutes displayed per second of playthrough
 	const [running, setRunning] = useState(false);
 	const [currentTime, setCurrentTime] = useState(0);
-	const [finished, setFinished] = useState(false);
+	// temporarily 4, should be updated based on reqs to the eventlog endpoint
+	const [totalEvents, setTotalEvents] = useState(4);
+	const [indexSeenUntil, setIndexSeenUntil] = useState(-1);
+	const currentEvents = useMemo(
+		() => eventsFromSvr.filter((e) => e.time <= currentTime),
+		[currentTime, eventsFromSvr],
+	);
+	const finished = currentEvents.length == totalEvents;
 
 	// update the time each tick
 	useEffect(() => {
@@ -54,8 +75,18 @@ const SimulationEventLogComponent = ({ uuid }) => {
 		}
 	}, [finished, speed, running]);
 
+	// updates index seen up until this current tick
+	useEffect(() => {
+		if (running)
+			// note that 600 has been chosen as it renders them within 0.4s (400ms) so this gives plenty of time to be
+			// processed in the event loop after they have fully rendered
+			setTimeout(() => setIndexSeenUntil(currentEvents.length - 1), 600);
+	}, [currentEvents.length, running]);
+
 	// References used:
-	// https://mantine.dev/core/slider/
+	// https://mantine.dev/ (several pages)
+	// https://stackoverflow.com/questions/62462316/how-do-i-make-a-button-fade-in-with-css-keyframes
+
 	return (
 		<>
 			<Title order={2}>Event Log</Title>
@@ -72,7 +103,7 @@ const SimulationEventLogComponent = ({ uuid }) => {
 						min={10}
 						max={100}
 						defaultValue={DEFAULT_PLAY_SPEED}
-						onChangeEnd={setSpeed}
+						onChange={setSpeed}
 						disabled={running}
 						marks={[
 							{ value: 10, label: "10" },
@@ -92,10 +123,30 @@ const SimulationEventLogComponent = ({ uuid }) => {
 					</Button>
 				</Stack>
 			</Card>
-
-			{currentEvents.map((e) => (
-				<Card key={e.id}></Card>
-			))}
+			{running ? (
+				<>
+					{currentEvents.map((e, i) => (
+						<Card
+							key={e.id}
+							style={
+								i > indexSeenUntil
+									? {
+											opacity: 0,
+											animationName: "fadeIn",
+											animationDuration: `0.4s`,
+											animationFillMode: "forwards",
+											animationTimingFunction: "ease",
+										}
+									: {}
+							}
+						>
+							{generateEventCard(e)}
+						</Card>
+					))}
+				</>
+			) : (
+				<></>
+			)}
 		</>
 	);
 };
