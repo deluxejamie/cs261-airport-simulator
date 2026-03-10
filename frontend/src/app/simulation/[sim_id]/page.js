06fc77f9a7b0f40088cc0b4857ebf2ab97f2b128
Jamie <deluxe@jaiii.me>
2026-03-10 13:53:04 +0000

    fix: uuid sanitisation is wrong

--- frontend/src/app/simulation/[sim_id]/page.js
+++ frontend/src/app/simulation/[sim_id]/page.js
@@ -1,7 +1,7 @@
 import SimulationOutcomeView from "@/components/simulation-outcome-page";
 import { redirect } from "next/navigation";
 
-const UUID_LENGTH = 32; // uuids are 32 characters long (v4 uuid)
+const UUID_LENGTH = 36; // uuids are 36 characters long (v4 uuid)
 
 export default async function Simulation({ params }) {
 	const { sim_id: simulationId } = await params;
