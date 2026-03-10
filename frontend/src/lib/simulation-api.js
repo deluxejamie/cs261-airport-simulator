a338f90118d9926cf6063233fd9f6385406b78d7
Jamie <deluxe@jaiii.me>
2026-03-10 13:53:16 +0000

    feat(simulation-api): mocked responses

--- frontend/src/lib/simulation-api.js
+++ frontend/src/lib/simulation-api.js
@@ -1,4 +1,6 @@
 "use client";
+import { EmergencyStatus } from "@/app/hooks";
+import { sleep } from "@/components/simulation-outcome-page";
 /**
  * Note that requests are made directly from the browser (not via the next server) for benefits discussed in design document
  * including easier ratelimiting, etc.
@@ -59,6 +61,10 @@ export async function createSimulation(config) {
  * @returns {String} Either the status "in_progress", "complete", or "unavailable" if the api is unreachable
  */
 export async function getSimulationStatus(uuid) {
+	if (uuid == "84f43a1c-9ec3-4139-885c-f929f3167cce") {
+		await sleep(300);
+		return "complete";
+	}
 	const safeID = encodeURIComponent(uuid);
 	try {
 		const res = await request(`/simulation/status/${safeID}`);
@@ -101,13 +107,89 @@ export async function getSimulationEventLog(uuid, offset = 0, count = 50) {
 				attributes: {
 					callsign: "BA-1",
 					holdMinutes: 3,
+					runwayNumber: 2,
 					arrivalDelay: 4,
 				},
 			},
+			{
+				id: 2,
+				eventType: EventTypes.TAKEOFF,
+				simulationId: uuid,
+				simTimestamp: 10,
+				attributes: {
+					callsign: "BA-2",
+					runwayNumber: 2,
+					waitMinutes: 3,
+					departureDelay: 5,
+				},
+			},
+			{
+				id: 3,
+				eventType: EventTypes.HOLDING,
+				simulationId: uuid,
+				simTimestamp: 50,
+				attributes: {
+					callsign: "EASYJET-3",
+					op: "not sure what this is",
+				},
+			},
+			{
+				id: 4,
+				eventType: EventTypes.DIVERSION,
+				simulationId: uuid,
+				simTimestamp: 50,
+				attributes: {
+					callsign: "EASYJET-4",
+					reason: "Flight has low fuel.",
+				},
+			},
+			{
+				id: 5,
+				eventType: EventTypes.CANCELLATION,
+				simulationId: uuid,
+				simTimestamp: 55,
+				attributes: {
+					callsign: "EASYJET-5",
+					reason: `No takeoff capacity after waiting 20 minutes`,
+					waitingMinutes: 20,
+				},
+			},
+			{
+				id: 6,
+				eventType: EventTypes.EMERGENCY,
+				simulationId: uuid,
+				simTimestamp: 65,
+				attributes: {
+					callsign: "QATAR-6",
+					emergencyStatus: "fuel",
+				},
+			},
+			{
+				id: 7,
+				eventType: EventTypes.RUNWAY_MODE,
+				simulationId: uuid,
+				simTimestamp: 80,
+				attributes: {
+					callsign: "QATAR-6",
+					emergencyStatus: "fuel",
+				},
+			},
+			{
+				id: 8,
+				eventType: EventTypes.RUNWAY_STATUS,
+				simulationId: uuid,
+				simTimestamp: 80,
+				attributes: {
+					callsign: "QATAR-6",
+					status: "not sure what this is",
+				},
+			},
 		];
+		await sleep(300); // mock the time taken for a request, 300ms
 		return {
 			events: mockedEvents.slice(offset, offset + count),
 			total_events: mockedEvents.length,
+			success: true,
 		};
 	}
 	try {
